72d580c03c08ed4424173aeb5846db3a499f5e74
Ayoub Kalil <aubkalil@gmail.com>
2017-11-21 00:13:05 +0000

    changed some content and added new icons for linkedin and github

--- dev/temp/scripts/portfolio-data.js
+++ dev/temp/scripts/portfolio-data.js
@@ -1,5 +1,5 @@
 var projects =[{  
-      name:"Website Sample 1",
+      name:"AKfolio Template",
       thumbnail:"assets/images/portfolio/portfolio_01.jpg",
       description:"Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.",
       image:"assets/images/portfolio/screenshot_01.jpg",
@@ -8,7 +8,7 @@ var projects =[{
    },
 
    {  
-      name:"Website Sample 2",
+      name:"wCreation",
       thumbnail:"assets/images/portfolio/portfolio_02.jpg",
       description:"Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.",
       image:"assets/images/portfolio/screenshot_02.jpg",
@@ -16,35 +16,35 @@ var projects =[{
       category:"webdesign"
    },
    {  
-      name:"Graphic Sample 1",
-      thumbnail:"assets/images/portfolio/graphic_thumb.jpg",
+      name:"Gameing Blog",
+      thumbnail:"assets/images/portfolio/portfolio_03.jpg",
       description:"Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.",
-      image:"assets/images/portfolio/graphic_01.jpeg",
+      image:"assets/images/portfolio/screenshot_03.jpg",
       liveLink:"",
-      category:"graphic"
+      category:"webdesign"
    },
    {  
-      name:"Logo Sample 1",
-      thumbnail:"assets/images/portfolio/logo_thumb.jpg",
+      name:"Simple Blog | kSoft",
+      thumbnail:"assets/images/portfolio/portfolio_04.jpg",
       description:"Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.",
-      image:"assets/images/portfolio/logo_01.jpeg",
-      liveLink:"",
-      category:"logo"
+      image:"assets/images/portfolio/screenshot_04.jpg",
+      liveLink:"http://ksoft.ga2h.com/",
+      category:"webdesign"
    },
    {  
-      name:"Graphic Sample 2",
-      thumbnail:"assets/images/portfolio/graphic_02_thumb.jpg",
+      name:"Free tuts",
+      thumbnail:"assets/images/portfolio/portfolio_05.jpg",
       description:"Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.",
-      image:"assets/images/portfolio/graphic_02.jpeg",
+      image:"assets/images/portfolio/screenshot_05.jpg",
       liveLink:"",
-      category:"graphic"
+      category:"webdesign"
    },
    {  
-      name:"Graphic Sample 3",
-      thumbnail:"assets/images/portfolio/graphic_03_thumb.jpg",
+      name:"Duvert",
+      thumbnail:"assets/images/portfolio/portfolio_06.jpg",
       description:"Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.",
-      image:"assets/images/portfolio/graphic_03.jpeg",
+      image:"assets/images/portfolio/screenshot_06.jpg",
       liveLink:"",
-      category:"graphic"
+      category:"webdesign"
    }
 ];
